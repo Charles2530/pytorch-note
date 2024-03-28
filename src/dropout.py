188bdd5637b7b13e81fe163ed9841bad62df91ec
charles <2569337619@qq.com>
2024-03-28 18:35:07 +0800

    丢弃法实现

---
+++ src/dropout.py
@@ -0,0 +1,57 @@
+import torch
+from torch import nn
+from d2l import torch as d2l
+from softmax_regression import train_ch3
+
+
+def dropout_layer(X, dropout):
+    assert 0 <= dropout <= 1
+    # In this case, all elements are dropped out
+    if dropout == 1:
+        return torch.zeros_like(X)
+    # In this case, all elements are kept
+    if dropout == 0:
+        return X
+    mask = (torch.randn(X.shape) > dropout).float()
+    return mask * X / (1.0 - dropout)
+
+
+x = torch.arange(16, dtype=torch.float32).reshape((2, 8))
+
+print(dropout_layer(x, 0.))
+
+num_inputs, num_outputs, num_hiddens1, num_hiddens2 = 784, 10, 256, 256
+dropout1, dropout2 = 0.2, 0.5
+
+
+class Net(nn.Module):
+    def __init__(self, num_inputs, num_outputs, num_hiddens1, num_hiddens2,
+                 is_training=True):
+        super(Net, self).__init__()
+        self.num_inputs = num_inputs
+        self.training = is_training
+        self.lin1 = nn.Linear(num_inputs, num_hiddens1)
+        self.lin2 = nn.Linear(num_hiddens1, num_hiddens2)
+        self.lin3 = nn.Linear(num_hiddens2, num_outputs)
+        self.relu = nn.ReLU()
+        self.dropout1 = nn.Dropout(dropout1)
+        self.dropout2 = nn.Dropout(dropout2)
+
+    def forward(self, X):
+        H1 = self.relu(self.lin1(X.reshape((-1, self.num_inputs))))
+        if self.training:
+            H1 = self.dropout1(H1)
+        H2 = self.relu(self.lin2(H1))
+        if self.training:
+            H2 = self.dropout2(H2)
+        out = self.lin3(H2)
+        return out
+
+
+net = Net(num_inputs, num_outputs, num_hiddens1, num_hiddens2)
+
+num_epochs, lr, batch_size = 10, 0.5, 256
+loss = nn.CrossEntropyLoss()
+train_iter, test_iter = d2l.load_data_fashion_mnist(batch_size)
+trainer = torch.optim.SGD(net.parameters(), lr=lr)
+train_ch3(net, train_iter, test_iter, loss, num_epochs, trainer)
