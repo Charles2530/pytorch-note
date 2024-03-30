4aafc5b8d30a6fc19f8ae2e92b6999353937bea4
charles <2569337619@qq.com>
2024-03-30 23:47:08 +0800

    finish

--- src/network/bert.py
+++ src/network/bert.py
@@ -1,4 +1,6 @@
+import random
 import torch
+import os
 from torch import nn
 from d2l import torch as d2l
 from transformer import EncoderBlock
@@ -132,3 +134,151 @@ class BERTModel(nn.Module):
         nsp_Y_hat = self.nsp(
             self.hidden(encoded_X[:, 0, :]))
         return encoded_X, mlm_Y_hat, nsp_Y_hat
+
+
+# BERT预处理
+d2l.DATA_HUB['bert.base'] = (
+    'https://apache-mxnet.s3-accelerate.amazonaws.com/'
+    'gluon/dataset/bert.base.zip',
+    '225d66f04ca45495337f4b2e4d331618f85f4e1f')
+
+
+def _read_wiki(data_dir):
+    file_name = os.path.join(data_dir, 'wiki.zh.simple.txt')
+    with open(file_name, 'r', encoding='utf-8') as f:
+        lines = f.readlines()
+    paragraphs = [line.strip() for line in lines]
+    random.shuffle(paragraphs)
+    return paragraphs
+
+
+def _get_next_sentence(sentence, next_sentence, paragraphs):
+    if random.random() < 0.5:
+        return sentence, paragraphs[random.randint(0, len(paragraphs) - 1)], 1
+    else:
+        return sentence, next_sentence, 0
+
+
+def _get_nsp_data_from_paragraph(paragraph, paragraphs, vocab, max_len):
+    nsp_data_from_paragraph = []
+    for i in range(len(paragraph) - 1):
+        tokens_a, tokens_b, is_next = _get_next_sentence(
+            paragraph[i], paragraph[i + 1], paragraphs)
+        # Consider 1 '<cls>' token and 2 '<sep>' tokens
+        if len(tokens_a) + len(tokens_b) + 3 > max_len:
+            continue
+        tokens, segments = get_tokens_and_segments(tokens_a, tokens_b)
+        nsp_data_from_paragraph.append((tokens, segments, is_next))
+    return nsp_data_from_paragraph
+
+
+def _replace_mlm_tokens(tokens, candidate_pred_positions, num_mlm_preds,
+                        vocab):
+    # Make a new copy of tokens for the input of a masked language model,
+    # where the input may contain replaced '<mask>' or random tokens
+    mlm_input_tokens = [token for token in tokens]
+    pred_positions_and_labels = []
+    random.shuffle(candidate_pred_positions)
+    for mlm_pred_position in candidate_pred_positions:
+        if len(pred_positions_and_labels) >= num_mlm_preds:
+            break
+        masked_token = mlm_input_tokens[mlm_pred_position]
+        # 80% of the time: replace the word with the '<mask>' token
+        if random.random() < 0.8:
+            mlm_input_tokens[mlm_pred_position] = '<mask>'
+        else:
+            # 10% of the time: replace the word with a random word
+            if random.random() < 0.5:
+                mlm_input_tokens[mlm_pred_position] = random.choice(vocab)
+            # 10% of the time: keep the word unchanged
+        pred_positions_and_labels.append(
+            (mlm_pred_position, masked_token))
+    return mlm_input_tokens, pred_positions_and_labels
+
+
+def _get_mlm_data_from_tokens(tokens, vocab):
+    candidate_pred_positions = []
+    for i, token in enumerate(tokens):
+        # Special tokens are not predicted in the masked language modeling
+        if token in ['<cls>', '<sep>']:
+            continue
+        candidate_pred_positions.append(i)
+    # 15% of random tokens are predicted in the masked language modeling
+    num_mlm_preds = max(1, round(len(tokens) * 0.15))
+    mlm_input_tokens, pred_positions_and_labels = _replace_mlm_tokens(
+        tokens, candidate_pred_positions, num_mlm_preds, vocab)
+    pred_positions_and_labels = sorted(pred_positions_and_labels,
+                                       key=lambda x: x[0])
+    pred_positions = [v[0] for v in pred_positions_and_labels]
+    mlm_pred_labels = [vocab[v[1]] for v in pred_positions_and_labels]
+    return vocab[mlm_input_tokens], pred_positions, mlm_pred_labels
+
+
+def _pad_bert_inputs(examples, max_len, vocab):
+    max_num_mlm_preds = round(max_len * 0.15)
+    all_token_ids, all_segments, valid_lens,  = [], [], []
+    all_pred_positions, all_mlm_weights, all_mlm_labels = [], [], []
+    nsp_labels = []
+    for (token_ids, pred_positions, mlm_pred_labels, segments,
+         is_next) in examples:
+        all_token_ids.append(torch.tensor(token_ids + [vocab['<pad>']] * (
+            max_len - len(token_ids)), dtype=torch.long))
+        all_segments.append(torch.tensor(segments + [0] * (
+            max_len - len(segments)), dtype=torch.long))
+        # `valid_lens` excludes count of '<pad>' tokens
+        valid_lens.append(torch.tensor(len(token_ids), dtype=torch.float32))
+        all_pred_positions.append(torch.tensor(pred_positions + [0] * (
+            max_num_mlm_preds - len(pred_positions)), dtype=torch.long))
+        # Predictions of padded tokens will be filtered out in the loss via
+        # multiplication of 0 weights
+        all_mlm_weights.append(
+            torch.tensor([1.0] * len(mlm_pred_labels) + [0.0] * (
+                max_num_mlm_preds - len(pred_positions)), dtype=torch.float32))
+        all_mlm_labels.append(torch.tensor(
+            [vocab[token] for token in mlm_pred_labels] + [0] * (
+                max_num_mlm_preds - len(mlm_pred_labels)), dtype=torch.long))
+        nsp_labels.append(torch.tensor(is_next, dtype=torch.long))
+    return (all_token_ids, all_segments, valid_lens, all_pred_positions,
+            all_mlm_weights, all_mlm_labels, nsp_labels)
+
+
+class _WikiTextDataset(torch.utils.data.Dataset):
+    def __init__(self, paragraphs, max_len):
+        # Input `paragraphs[i]` is a list of sentence strings representing a
+        # paragraph; while output `paragraphs[i]` is a list of sentences
+        # representing a paragraph, where each sentence is a list of tokens
+        paragraphs = [d2l.tokenize(paragraph) for paragraph in paragraphs]
+        sentences = [sentence for paragraph in paragraphs
+                     for sentence in paragraph]
+        self.vocab = d2l.Vocab(sentences, min_freq=5, reserved_tokens=[
+            '<pad>', '<mask>', '<cls>', '<sep>'])
+        examples = []
+        for sentence in sentences:
+            # Consider 1 '<cls>' token and 2 '<sep>' tokens
+            if len(sentence) + 3 > max_len:
+                continue
+            examples.append(
+                _get_mlm_data_from_tokens(['<cls>'] + sentence + ['<sep>'],
+                                          self.vocab))
+        self.all_token_ids, self.all_segments, self.valid_lens, \
+            self.all_pred_positions, self.all_mlm_weights, self.all_mlm_labels, \
+            self.nsp_labels = _pad_bert_inputs(examples, max_len, self.vocab)
+
+    def __getitem__(self, idx):
+        return (self.all_token_ids[idx], self.all_segments[idx],
+                self.valid_lens[idx], self.all_pred_positions[idx],
+                self.all_mlm_weights[idx], self.all_mlm_labels[idx],
+                self.nsp_labels[idx])
+
+    def __len__(self):
+        return len(self.all_token_ids)
+
+
+def load_data_wiki(batch_size, max_len):
+    num_workers = d2l.get_dataloader_workers()
+    data_dir = d2l.download_extract('bert.base')
+    paragraphs = _read_wiki(data_dir)
+    train_set = _WikiTextDataset(paragraphs, max_len)
+    train_iter = torch.utils.data.DataLoader(train_set, batch_size,
+                                             shuffle=True, num_workers=num_workers)
+    return train_iter, train_set.vocab
